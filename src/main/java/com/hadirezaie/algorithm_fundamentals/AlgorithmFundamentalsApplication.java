e15394e588215095ee6fa7f036e2f4138ff78b68
Hadi <hadi.rezaie2015@gmail.com>
2025-09-29 16:40:46 +0430

    initial commit

---
+++ src/main/java/com/hadirezaie/algorithm_fundamentals/AlgorithmFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.hadirezaie.algorithm_fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hadirezaie/algorithm_fundamentals/AlgorithmFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadirezaie.algorithm_fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
