d7878f9bdc49f9909476e550aa29a86a622d7d00
Hadi <hadi.rezaie2015@gmail.com>
2025-10-03 22:45:55 +0430

    updated.

--- src/main/java/com/hadirezaie/algorithm_fundamentals/AlgorithmFundamentalsApplication.java
+++ src/main/java/com/hadirezaie/algorithm_fundamentals/AlgorithmFundamentalsApplication.java
@@ -10,13 +10,13 @@ public class AlgorithmFundamentalsApplication {
 		SpringApplication.run(AlgorithmFundamentalsApplication.class, args);
 		// finding the largest number in an array
 		int[] testNumbers = { 3, 7, 2, 9, 1, 8, 12, 4 };
-		int result = Maximum.findMaximum(testNumbers);
+		int result = FindLargestNumber.findLargestNum(testNumbers);
 		System.out.println("Largest number: " + result);
 
 		System.out.println("\n" + "=".repeat(40) + "\n");
 
 		// finding the largest number with logging
-		Maximum.findMaximumWithLogging(testNumbers);
+		FindLargestNumber.findLargestNumWithLogging(testNumbers);
 	}
 
 }
